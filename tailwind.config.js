43eeb660939bfc3224a7006fc232cad5eca82a20
sadequlfakir <sadikul0140@gmail.com>
2022-10-08 15:54:22 +0600

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6F3E8D",
+        secondary: "#E0914F",
+        "secondary-100": "#E0914F",
+        "secondary-200": "#EEA86D",
+      },
+    },
   },
   plugins: [],
-}
+};
