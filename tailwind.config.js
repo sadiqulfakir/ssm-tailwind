98da2a8d7d9f8fe2459034c90fba7bd7dd51cc45
sadequlfakir <sadikul0140@gmail.com>
2022-10-10 22:40:11 +0600

    footer-updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 // const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
@@ -6,13 +7,32 @@ module.exports = {
     extend: {
       colors: {
         primary: "#6F3E8D",
+        "primary-100": "#4C2961",
         secondary: "#E0914F",
         "secondary-100": "#E0914F",
         "secondary-200": "#EEA86D",
+        "red-orange": "#C93305"
       },
       fontSize: {
-        '3.5xl': '32px'
-      }
+        '3.5xl': '32px',
+        '15px' : '15px'
+      },
+      spacing: {
+        '34': '34%',
+        '1-rem': '-1.30rem',
+        'r-rem': '-1rem',
+        't-50': '-50%'
+      },
+      fontFamily: {
+      'popin': 'Poppins, sans-serif',
+      },
+      backgroundImage: {
+        speak: "url('/src/assets/photo/speak.png')",
+      },
+    },
+    screens: {
+      'xs': '480px',
+      ...defaultTheme.screens,
     },
   },
   plugins: [],
